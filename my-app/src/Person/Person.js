225b590dfd67206d653723642cb31b48d0f1dd99
Yash Maheshwari <yashmaheshwari696@gmail.com>
2020-09-04 14:47:23 +0530

    third commit

--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Person.css'
+//import styled from 'styled-components';
 import Radium from 'radium';
 
 const person = (props) => {
+
+    const style = {
+        '@media (min-width: 500px)'{
+            width: '450px'
+        }
+    }
+
     return (
-        <div className="Person">
+        <div className="Person" style={style}>    
             <h3 onClick={props.click}>I am {props.name} and I'm {props.age} years old .!!</h3>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}></input>
